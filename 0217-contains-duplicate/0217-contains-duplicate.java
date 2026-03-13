d345806d2abb75387f66142a7f602888da912692
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-03-13 09:21:39 +0530

    Time: 25 ms (13.37%), Space: 76.5 MB (97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer>s=new HashSet<>();
-
-        for(int i:nums){
-            if(!s.add(i)){
-                
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
                 return true;
-            }        
         }
         return false;
     }
