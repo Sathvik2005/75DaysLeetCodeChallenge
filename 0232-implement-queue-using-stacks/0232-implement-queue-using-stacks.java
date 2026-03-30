8011a15755eef5b7ab0c9efd32663994edb97836
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-03-30 12:35:44 +0530

    Time: 0 ms (100%), Space: 42.4 MB (92.33%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,36 @@
+class MyQueue {
+     Stack<Integer> s1; // Input stack
+     Stack<Integer> s2; // Output stack
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+    }
+    
+    public int pop() {
+        move();
+        return s2.pop();
+    }
+    
+    public int peek() {
+        move();
+        return s2.peek();
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+
+    private void move() {
+        // Only move elements if the output stack is empty
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+    }
+}
