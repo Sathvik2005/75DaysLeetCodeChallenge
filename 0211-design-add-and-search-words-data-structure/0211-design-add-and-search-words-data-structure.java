b555f5c136e30224c25fab0dadfccc7939d4c92a
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-04-25 22:29:45 +0530

    Time: 181 ms (88.58%), Space: 274.2 MB (53.06%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,56 @@
+class WordDictionary {
+    private class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+            curr = curr.children[index];
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        return searchInNode(word, 0, root);
+    }
+
+    private boolean searchInNode(String word, int index, TrieNode node) {
+        if (index == word.length()) {
+            return node.isEndOfWord;
+        }
+
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (TrieNode child : node.children) {
+                if (child != null && searchInNode(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            int charIdx = c - 'a';
+            TrieNode child = node.children[charIdx];
+            return child != null && searchInNode(word, index + 1, child);
+        }
+    }
+}
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
