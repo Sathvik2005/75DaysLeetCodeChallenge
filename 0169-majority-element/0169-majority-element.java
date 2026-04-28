daf4a1503e4693051ab70672386c363e6aeba482
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-04-28 14:15:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,12 +4,11 @@ class Solution {
         int count = 0;
 
         for (int num : nums) {
-            // If count is 0, we pick the current number as the new candidate
+           ]
             if (count == 0) {
                 candidate = num;
             }
             
-            // Increment count if num matches candidate, else decrement
             count += (num == candidate) ? 1 : -1;
         }
 
