0228b0b5498fead4fe5be460aa8516cee0b95705
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-05-24 13:13:36 +0530

    Time: 630 ms (30.66%), Space: 19.5 MB (80.87%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        # The root is the smallest of the k largest, which is the kth largest
+        return min_heap[0]
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
