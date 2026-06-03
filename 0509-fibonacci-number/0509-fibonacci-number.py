c190f60739260bb64dd85a138b914f3691364839
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-06-03 23:22:52 +0530

    Time: 6 ms (98.51%), Space: 12.5 MB (16.73%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        prev2, prev1 = 0, 1
+        
+        for _ in range(2, n + 1):
+            current = prev1 + prev2
+            prev2 = prev1
+            prev1 = current
+            
+        return prev1
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
