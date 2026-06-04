951c26cebf80f98d22d9cc128074b85b621c49ca
Sathvik <150327940+Sathvik2005@users.noreply.github.com>
2026-06-04 18:20:58 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.18%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev1 = 0  # Max profit up to the previous house
+        prev2 = 0  # Max profit up to two houses ago
+        
+        for num in nums:
+            # Decide whether to rob the current house or skip it
+            current = max(prev1, prev2 + num)
+            
+            # Update variables for the next iteration
+            prev2 = prev1
+            prev1 = current
+            
+        return prev1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
